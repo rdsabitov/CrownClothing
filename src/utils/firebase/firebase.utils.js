1a920d6b39e3a59fa9d91494a6988817ad36fe44
Rishat Sabitov <rdsabitov777@gmail.com>
2022-10-26 09:41:29 -0400

    Added firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
 import  { initializeApp } from 'firebase/app'
 import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
-
+import { getFirestore, doc, getDoc, setDoc } from '@firebase/firestore';
+import { Logger } from '@firebase/logger';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -23,4 +24,28 @@ const firebaseConfig = {
 
   export const auth = getAuth();
   export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
-  
+
+  export const db = getFirestore();
+
+  export const createUserDocumentFromAuth = async(userAuth) => { 
+      const userDocRef = doc(db, 'users', userAuth.uid);
+
+      const userSnapshot = await getDoc(userDocRef)
+
+      if(!userSnapshot.exists()) { 
+          const {displayName, email} = userAuth;
+          const createAt = new Date();
+
+          try { 
+              await setDoc(userDocRef, { 
+                  displayName,
+                  email,
+                  createAt
+
+              }); 
+          } catch (error) { 
+              console.log('error creating the user' + error.message);
+          }
+      } 
+      return userDocRef;
+  }
