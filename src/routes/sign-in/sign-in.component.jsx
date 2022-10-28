26a525af74dd765f8c4a6edbcca1f9a38dff3c6e
Rishat Sabitov <rdsabitov777@gmail.com>
2022-10-28 10:24:15 -0400

    Includes Sign Up Form updates

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,8 +1,8 @@
 
 import {signInWithGooglePopup, createUserDocumentFromAuth} from '../../utils/firebase/firebase.utils.js'
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component.jsx';
 
 const SignIn = () => { 
-   
     const logGoogleUser = async() => {
          const {user} = await signInWithGooglePopup();
          const userDocRef = await createUserDocumentFromAuth(user);
@@ -13,7 +13,8 @@ return (
     <div>
         <h1>Sign In Page</h1>
         <button onClick={logGoogleUser}>Sign in with Google Popup</button>
+        <SignUpForm />
     </div>
-)
-}
+)}
+
 export default SignIn;
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,7 +1,6 @@
 import  { initializeApp } from 'firebase/app'
-import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
+import { getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider, createUserWithEmailAndPassword } from 'firebase/auth';
 import { getFirestore, doc, getDoc, setDoc } from '@firebase/firestore';
-import { Logger } from '@firebase/logger';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,17 +16,21 @@ const firebaseConfig = {
   const firebaseApp = initializeApp(firebaseConfig);
 
 
-  const provider = new GoogleAuthProvider();
-  provider.setCustomParameters({
+  const googleProvider = new GoogleAuthProvider();
+  googleProvider.setCustomParameters({
       prompt: "select_account"
   })
 
   export const auth = getAuth();
-  export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+  export const signInWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+  export const signInWithGoogleRedirect = () => signInWithRedirect(auth, googleProvider)
 
   export const db = getFirestore();
 
-  export const createUserDocumentFromAuth = async(userAuth) => { 
+  export const createUserDocumentFromAuth = async(userAuth,additionalInformation={}) => { 
+      if(!userAuth) { 
+          return ;
+      }
       const userDocRef = doc(db, 'users', userAuth.uid);
 
       const userSnapshot = await getDoc(userDocRef)
@@ -40,7 +43,8 @@ const firebaseConfig = {
               await setDoc(userDocRef, { 
                   displayName,
                   email,
-                  createAt
+                  createAt,
+                  ...additionalInformation,
 
               }); 
           } catch (error) { 
@@ -48,4 +52,12 @@ const firebaseConfig = {
           }
       } 
       return userDocRef;
+  }
+
+
+  export const createAuthUserWithEmailAndPassword = async (email, password) => { 
+      if(!email||!password) { 
+          return;
+      }
+      return await createUserWithEmailAndPassword(auth, email, password)
   }
