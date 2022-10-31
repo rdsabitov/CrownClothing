37aa1d96b299cce3fe8d3b27c286c2893666dfc9
Rishat Sabitov <rdsabitov777@gmail.com>
2022-10-31 10:51:36 -0400

    Added sign-up button in sign-in page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -1,6 +1,8 @@
 import {useState} from 'react'
 import { createAuthUserWithEmailAndPassword, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils';
 import FormInput from '../form-input/form-input.component';
+import './sign-up-form.styles.scss'
+import Button from '../button/button.component'
 
 const defaultFormFields = { 
     displayName: '',
@@ -48,8 +50,9 @@ const SignUpForm = () => {
 
 
     return ( 
-        <div>
-            <h1>Sign up with your email and password</h1>
+        <div className='sign-up-container'>
+            <h2>Don't have an account?</h2>
+            <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}> 
                 <FormInput
                     label='Display Name'
@@ -85,7 +88,7 @@ const SignUpForm = () => {
                     name='confirmPassword'
                     value={confirmPassword}
                  />
-                <button type='submit'>Sign Up</button>
+                <Button buttonType='google' type='submit'>Sign Up</Button>
                 
             </form>
         </div> 
