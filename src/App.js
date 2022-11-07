50ae38ad19d4c8766453497f172e7ce0ff26e1c8
Rishat Sabitov <rdsabitov777@gmail.com>
2022-11-07 13:04:32 -0500

    Added category preview, categories preview, category routes

--- src/App.js
+++ src/App.js
@@ -6,25 +6,16 @@ import Navigation from './routes/navigation/navigation.component';
 import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
-// const Navigation = () => { 
-//   return ( 
-//     <div>
-//       <div>
-//         <h1>I am the navigation bar</h1>
-//       </div>
-//       <Outlet/>
-//     </div>
-//   )
-// }
+
+
 const App = () => {
   return ( 
   <Routes> 
-    <Route path='/' element={ <Navigation/>} >
+    <Route path='/' element={ <Navigation />} >
       <Route index  element={<Home/>} /> 
-      <Route path='shop' element={<Shop />} />
+      <Route path='shop/*' element={<Shop />} />
       <Route path='auth' element={<Authentication/>}/>
       <Route path='checkout' element={<Checkout/>}/>
-      {/* //<Route path='shop' element={<Shop />} /> */}
    </Route>
 
   </Routes>
